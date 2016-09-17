e31fee6f58ce71c242f2455b45c71f5e79270f4c
suprita shankar <suprita.shankar@gmail.com>
2016-09-17 12:26:56 -0700

    add models

--- todoapp/todos/models.py
+++ todoapp/todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    todo_text = models.CharField(max_length=200)
+    completed = models.BooleanField()
+    created_date = models.DateTimeField('created date')
+
+    def __str__(self):
+        return self.todo_text
