fdf970819abba7507567f335e0091a5b94dfdbbe
suprita shankar <suprita.shankar@gmail.com>
2016-09-18 10:41:50 -0700

    give a default date value

--- todoapp/todos/models.py
+++ todoapp/todos/models.py
@@ -1,11 +1,13 @@
+import django
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 #TODO: Add created_by to the models
 class Todo(models.Model):
     todo_text = models.CharField(max_length=200)
     completed = models.BooleanField()
-    created_date = models.DateTimeField('created date')
+    created_date = models.DateTimeField(default=django.utils.timezone.now)
 
     def __str__(self):
         return self.todo_text
