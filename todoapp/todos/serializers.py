eb07fa9e6a8f758a3e2bce52cc91c6c398c60f25
suprita shankar <suprita.shankar@gmail.com>
2016-09-18 10:41:22 -0700

    add some custom validation

--- todoapp/todos/serializers.py
+++ todoapp/todos/serializers.py
@@ -2,12 +2,16 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from models import Todo
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class TodoSerializer(serializers.ModelSerializer):
+    def validate_todo_text(self, value):
+        if not len(value.split()) > 1:
+            raise serializers.ValidationError("Todo task should have at least 2 words")
+        return value
     class Meta:
         model = Todo
         fields = ('todo_text', 'completed')
