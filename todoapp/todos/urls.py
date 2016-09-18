d21bf1cd9080539d3228fdc8f0fce619ebde3249
suprita shankar <suprita.shankar@gmail.com>
2016-09-18 11:21:36 -0700

    use API View

--- todoapp/todos/urls.py
+++ todoapp/todos/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 from rest_framework import routers
 from django.conf.urls import include
 from . import views
-
+from todos.views import TodoView
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'todos', views.TodoViewSet)
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^', include(router.urls)),
+    url(r'^todos', TodoView.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- todoapp/todos/views.py
+++ todoapp/todos/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from serializers import UserSerializer, TodoSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from models import Todo
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,12 +15,21 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
-class TodoViewSet(viewsets.ModelViewSet):
+class TodoView(APIView):
     """
     API endpoint that allows todos to be viewed or edited
     """
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+    def get(self, request, format=None):
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def index(request):
